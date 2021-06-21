512332b99bf6cd75eb716cf8c24cd4e7e6766234
sang5c <skah321@gmail.com>
2021-06-21 22:44:01 +0900

    add welcome to test

--- sowjsrkr.go
+++ sowjsrkr.go
@@ -14,7 +14,14 @@ const (
 func main() {
 	router := gin.Default()
 	router.GET("/", helloWorld)
+	router.GET("/test", welcome)
 	router.Run()
+}
+
+func welcome(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"HI": "WELCOME",
+	})
 }
 
 func helloWorld(context *gin.Context) {
